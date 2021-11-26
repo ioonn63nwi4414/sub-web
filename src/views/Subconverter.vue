2a4887ad8a3568df843b03660c8d50512a9e137b
ioonn63nwi4414 <90600952+ioonn63nwi4414@users.noreply.github.com>
2021-11-26 19:56:30 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "http://polished-moon-5d49.jth445600.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
